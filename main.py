7eeb1700af6c0aded0626bfbbd95fc3f314321dc
eleora-h <eleora.hamming@gmail.com>
2023-03-05 21:24:14 -0500

    Add more scenes

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # Author: Eleora Hamming
 # Main scene
+
+import scenes.intro_scene as intro_scene
+
+if __name__ == "__main__":
+    while True:
+        intro = """
+        Welcome to 
+        """
---
+++ scenes/intro_scene.py
@@ -0,0 +1,1 @@
+# Author: Eleora Hamming
